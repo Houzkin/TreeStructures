78c38927891b95afe45570e9fcc5e11f6bed9d56
houzkin <pr.peony@gmail.com>
2023-12-18 18:43:40 +0900

    a

--- src/TreeStructure/Tree/NAryTreeNode.cs
+++ src/TreeStructure/Tree/NAryTreeNode.cs
@@ -14,16 +14,25 @@ namespace TreeStructures.Tree {
         /// <summary>コンストラクタ</summary>
         /// <param name="nary"></param>
         protected NAryTreeNode(int nary) :base() {
+            AryCount = nary;
             for(int i = 0; i < nary; i++) {
                 this.AddAction(this.ChildNodes, null);
             }
         }
         /// <inheritdoc/>
         protected override IList<T> ChildNodes { get; } = new List<T>();
+        protected int AryCount { get; init; }
         /// <summary>子ノードの入替処理を実行するプロセス</summary>
         protected virtual void SetChildProcess(int index, T value) {
             base.RemoveChildProcess(ChildNodes.ElementAt(index));
             base.InsertChildProcess(index, value);
+        }
+        protected override void InsertChildProcess(int index, T child) {
+            
+
+        }
+        protected override void AddChildProcess(T child) {
+            
         }
         /// <summary>子ノードの削除処理を実行するプロセス</summary>
         protected override void RemoveChildProcess(T child) {
@@ -43,6 +52,13 @@ namespace TreeStructures.Tree {
             var idx = ChildNodes/*.ToList()*/.IndexOf(null);
             if(0<=idx) 
                 SetChildProcess(idx, child);
+            return Self;
+        }
+        /// <summary>インデックスを指定して子ノードを追加する</summary>
+        /// <returns>現在のノード</returns>
+        public T SetChild(int index, T child) {
+            if (!this.CanAddChildNode(child)) return Self;
+            SetChildProcess(index, child);
             return Self;
         }
         /// <summary>ノードを削除する</summary>
