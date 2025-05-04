ef6c2e782484d239c43a6adac79bce67f8e12736
Houzkin <pr.peony@gmail.com>
2025-05-04 19:02:20 +0900

    a

--- src/TreeStructuresStd/Events/PropertyChangeProxy.cs
+++ src/TreeStructuresStd/Events/PropertyChangeProxy.cs
@@ -16,8 +16,8 @@ namespace TreeStructures.Events {
 	/// It helps manage property changes and notifications efficiently.
 	///</summary>
 	public class PropertyChangeProxy {
-        private static readonly ConcurrentDictionary<string,PropertyChangedEventArgs> _eventArgsCash = new();
-        Action<string> onAction;
+		private static readonly ConcurrentDictionary<string, PropertyChangedEventArgs> _eventArgsCash = new();
+		Action<string> onAction;
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PropertyChangeProxy"/> class
 		/// that delegates notifications to the specified event handler.
@@ -25,10 +25,10 @@ namespace TreeStructures.Events {
 		/// <param name="sender">The object that owns this proxy.</param>
 		/// <param name="handlerGetter">A function that retrieves the event handler for notifications.</param>
 		public PropertyChangeProxy(object? sender, Func<PropertyChangedEventHandler?> handlerGetter) {
-            this.onAction = new(name => {
-                var hdlr = handlerGetter();
-                hdlr?.Invoke(sender,toArgs(name));
-            });
+			this.onAction = new(name => {
+				var hdlr = handlerGetter();
+				hdlr?.Invoke(sender, toArgs(name));
+			});
 		}
 		/// <summary>
 		/// Initializes a new instance of the <see cref="PropertyChangeProxy"/> class
@@ -36,10 +36,10 @@ namespace TreeStructures.Events {
 		/// </summary>
 		/// <param name="raiseAction">An action to invoke property change notifications.</param>
 		public PropertyChangeProxy(Action<PropertyChangedEventArgs> raiseAction) {
-            this.onAction = new(name => { raiseAction(toArgs(name)); });
-        }
-        private static PropertyChangedEventArgs toArgs(string name) 
-            => _eventArgsCash.GetOrAdd(name, n => new PropertyChangedEventArgs(n));
+			this.onAction = new(name => { raiseAction(toArgs(name)); });
+		}
+		private static PropertyChangedEventArgs toArgs(string name)
+			=> _eventArgsCash.GetOrAdd(name, n => new PropertyChangedEventArgs(n));
 
 		/// <summary>
 		/// Sets the given storage value and sends a property change notification if the value has changed.
@@ -50,10 +50,10 @@ namespace TreeStructures.Events {
 		/// <param name="propertyName">The name of the property (automatically determined).</param>
 		/// <returns>A result indicating whether the value was changed.</returns>
 		public ResultWithValue<PropertyChangeProxy> TrySetAndNotify<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null) {
-            if (EqualityComparer<T>.Default.Equals(storage, value)) return new ResultWithValue<PropertyChangeProxy>(false, this);
+			if (EqualityComparer<T>.Default.Equals(storage, value)) return new ResultWithValue<PropertyChangeProxy>(false, this);
 			storage = value;
 			Notify(propertyName);
-            return new ResultWithValue<PropertyChangeProxy>(true, this);
+			return new ResultWithValue<PropertyChangeProxy>(true, this);
 		}
 		/// <summary>
 		/// Sends a property change notification for the specified property name.
@@ -62,50 +62,50 @@ namespace TreeStructures.Events {
 		/// <returns>The current instance of <see cref="PropertyChangeProxy"/>.</returns>
 		public PropertyChangeProxy Notify([CallerMemberName] string? propertyName = null) {
 			if (string.IsNullOrEmpty(propertyName)) return this;
-            this.onAction(propertyName);
-            return this;
+			this.onAction(propertyName);
+			return this;
 		}
 	}
 
 
 	///// <summary>Supports the implementation of <see cref="INotifyPropertyChanged"/>.</summary>
 	//public class PropertyChangeProxy /*:INotifyPropertyChanged */{
- //       object? sender;
- //       /// <summary>Initializes a new instance.</summary>
- //       /// <param name="sender">The instance to be specified as the sender when the event is raised.</param>
- //       public PropertyChangeProxy(object? sender) {
- //           this.sender = sender;
- //       }
- //       /// <summary>Performs value change and issues change notifications.</summary>
- //       /// <typeparam name="T">The type of the value.</typeparam>
- //       /// <param name="storage">The storage for the value.</param>
- //       /// <param name="value">The new value.</param>
- //       /// <param name="propertyName">The name of the property (automatically provided by the compiler).</param>
- //       /// <returns>True if there was a change in the value, false otherwise.</returns>
- //       public bool TrySetAndNotify<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null) {
- //           if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
- //           storage = value;
- //           Notify(propertyName);
- //           return true;
- //       }
- //       /// <summary>Issues property change notification.</summary>
- //       /// <param name="propertyName">The name of the property.</param>
- //       public virtual void Notify([CallerMemberName] string? propertyName = null) {
- //           if (string.IsNullOrEmpty(propertyName)) return;
- //           var handler = Changed;
- //           handler?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
- //       }
- //       /// <summary>Removes all registered handlers.</summary>
- //       public void ClearHandler() { 
- //           Changed = null;
- //       }
- //       //internal int HandlerCount =>
- //       //        PropertyChanged.GetLength();
- //       //event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged {
- //       //    add { PropertyChanged += value; }
- //       //    remove { PropertyChanged -= value; }
- //       //}
- //       /// <summary>Occurs when a property is changed.</summary>
- //       public event PropertyChangedEventHandler? Changed;
- //   }
+	//       object? sender;
+	//       /// <summary>Initializes a new instance.</summary>
+	//       /// <param name="sender">The instance to be specified as the sender when the event is raised.</param>
+	//       public PropertyChangeProxy(object? sender) {
+	//           this.sender = sender;
+	//       }
+	//       /// <summary>Performs value change and issues change notifications.</summary>
+	//       /// <typeparam name="T">The type of the value.</typeparam>
+	//       /// <param name="storage">The storage for the value.</param>
+	//       /// <param name="value">The new value.</param>
+	//       /// <param name="propertyName">The name of the property (automatically provided by the compiler).</param>
+	//       /// <returns>True if there was a change in the value, false otherwise.</returns>
+	//       public bool TrySetAndNotify<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null) {
+	//           if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
+	//           storage = value;
+	//           Notify(propertyName);
+	//           return true;
+	//       }
+	//       /// <summary>Issues property change notification.</summary>
+	//       /// <param name="propertyName">The name of the property.</param>
+	//       public virtual void Notify([CallerMemberName] string? propertyName = null) {
+	//           if (string.IsNullOrEmpty(propertyName)) return;
+	//           var handler = Changed;
+	//           handler?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+	//       }
+	//       /// <summary>Removes all registered handlers.</summary>
+	//       public void ClearHandler() { 
+	//           Changed = null;
+	//       }
+	//       //internal int HandlerCount =>
+	//       //        PropertyChanged.GetLength();
+	//       //event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged {
+	//       //    add { PropertyChanged += value; }
+	//       //    remove { PropertyChanged -= value; }
+	//       //}
+	//       /// <summary>Occurs when a property is changed.</summary>
+	//       public event PropertyChangedEventHandler? Changed;
+	//   }
 }
